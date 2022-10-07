c91f8800d5e3d88f84b33fee407b6431eb0d56f7
alex3r45u <alexanderpalme07@gmail.com>
2022-10-07 16:02:02 +0200

    added learn views

--- PocketLearn.Win/MVVM/ViewModel/AnswerVM.cs
+++ PocketLearn.Win/MVVM/ViewModel/AnswerVM.cs
@@ -9,29 +9,47 @@ namespace PocketLearn.Win.MVVM.ViewModel
 {
     public class AnswerVM : ObservableObject
     {
+        public CardContent QuestionContent { get => QuestionContent; set
+            {
+                QuestionContent = value;
+                RaisePropertyChanged();
+            }
+        }
+        public CardContent AnswerContent { get => AnswerContent; set { 
+                AnswerContent = value;
+                RaisePropertyChanged();
+            } 
+        }
         public RelayCommand HardAnswer { get; set; } = new RelayCommand(_ =>
         {
-
+            MainWindowVM.Instance.AnswerVM.Answer(CardDifficulty.Hard);
         });
         public RelayCommand MediumAnswer { get; set; } = new RelayCommand(_ =>
         {
-            
+            MainWindowVM.Instance.AnswerVM.Answer(CardDifficulty.Medium);
         });
         public RelayCommand OKAnswer { get; set; } = new RelayCommand(_ =>
         {
-           
+            MainWindowVM.Instance.AnswerVM.Answer(CardDifficulty.OK);
         });
         public RelayCommand EasyAnswer { get; set; } = new RelayCommand(_ =>
         {
-            
+            MainWindowVM.Instance.AnswerVM.Answer(CardDifficulty.Easy);
         });
-        private void Answer(CardDifficulty difficulty)
+        public void Answer(CardDifficulty difficulty)
         {
-
+            Project.CardInput(difficulty);
+            if(Project.ShouldLearn())
+            {
+                MainWindowVM.Instance.CurrentView = MainWindowVM.Instance.QuestionVM;
+                MainWindowVM.Instance.QuestionVM.NextCard();
+            }
         }
         public void Update()
         {
-
+            var cardContents = Project.GetActiveCardContents();
+            QuestionContent = cardContents.Item1;
+            AnswerContent = cardContents.Item2;
         }
         LearnProject Project { get; set; }
         public AnswerVM()
