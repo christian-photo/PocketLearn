c36e3d733e0c2ae0090bedb48ba581f4026128dd
alex3r45u <alexanderpalme07@gmail.com>
2022-10-12 14:41:38 +0200

    asdf

--- PocketLearn/PocketLearn/Core/Learning/LearnProject.cs
+++ PocketLearn/PocketLearn/Core/Learning/LearnProject.cs
@@ -15,7 +15,7 @@ namespace PocketLearn.Core.Learning
         private List<LearnCard> _cards;
         public List<LearnCard> Cards { get => _cards; set { _cards = value; } }
         public ProjectConfig ProjectConfig { get; set; }
-        public Guid ProjectID { get; private set; } = Guid.NewGuid();
+        public Guid ProjectID { get; private set; };
 
         private List<LearnCard> hardCards = new List<LearnCard>();
         private List<LearnCard> mediumCards = new List<LearnCard>();
@@ -30,6 +30,7 @@ namespace PocketLearn.Core.Learning
         {
             this.CreationTime = CreationDate;
             this.HasToBeCompleted = HasToBeCompletet;
+            ProjectID = Guid.NewGuid();
         }
 
         public void SetCards(List<LearnCard> cards)
