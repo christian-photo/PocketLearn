5514a3a39baecaf65fcea31613701eed5e328549
alex3r45u <alexanderpalme07@gmail.com>
2022-10-28 13:34:22 +0200

    Started mobile app

--- PocketLearn/PocketLearn/Core/Learning/LearnProject.cs
+++ PocketLearn/PocketLearn/Core/Learning/LearnProject.cs
@@ -15,10 +15,6 @@ namespace PocketLearn.Core.Learning
         private List<LearnCard> _cards;
         public List<LearnCard> Cards { get => _cards; set { _cards = value; } }
         public ProjectConfig ProjectConfig { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 00e93d22dba5256841522ea9ad6aafb5dc5c9ee8
         public Guid ProjectID { get; private set; }
 
         private List<LearnCard> hardCards = new List<LearnCard>();
