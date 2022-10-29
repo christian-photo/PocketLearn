f5ee58e0f34a77cf026f3887201cf301cd6d46e4
rennmaus-coder <christian@palm-family.de>
2022-10-29 11:55:10 +0200

    remove unused package

--- PocketLearn.Win/App.xaml.cs
+++ PocketLearn.Win/App.xaml.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Toolkit.Uwp.Notifications;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
 using PocketLearn.Public.Core.Config;
 using PocketLearn.Win.Core;
 using PocketLearn.Win.Core.Notification;
 using PocketLearn.Win.MVVM.ViewModel;
 using System.IO;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace PocketLearn.Win
 {
