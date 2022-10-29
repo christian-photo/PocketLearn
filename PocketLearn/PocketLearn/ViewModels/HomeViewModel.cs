b50a128a90f79023a5c0d1213bb85678e41f6edf
alex3r45u <alexanderpalme07@gmail.com>
2022-10-29 21:08:14 +0200

    Trying to implement Projectitems

--- PocketLearn/PocketLearn/ViewModels/HomeViewModel.cs
+++ PocketLearn/PocketLearn/ViewModels/HomeViewModel.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using PocketLearn.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace PocketLearn.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+
+        public ObservableCollection<ProjectItem> ProjectItems { get; }
+        public Command<Item> ProjectItemTapped { get; }
+
+        public HomeViewModel()
+        {
+            Title = "Home";
+            ProjectItems = new ObservableCollection<ProjectItem>();
+            ProjectItemTapped = new Command<Item>(OnItemTapped);
+        }
+
+        void OnItemTapped(ProjectItem item)
+        {
+            if (item == null)
+                return;
+            //TODO got to learn view
+        }
     }
 }
