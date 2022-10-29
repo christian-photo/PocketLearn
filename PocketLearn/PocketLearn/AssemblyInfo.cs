afa93b049609f88b04cf7e06354afe5d4613d8ab
alex3r45u <alexanderpalme07@gmail.com>
2022-10-29 20:49:49 +0200

    items

--- PocketLearn/PocketLearn/AssemblyInfo.cs
+++ PocketLearn/PocketLearn/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Android.App;
 using Xamarin.Forms.Xaml;
 
+[assembly: UsesFeature("android.hardware.camera", Required = true)]
+[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
