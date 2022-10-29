db25a4a4b14832c85f27e4240851149a4ea497da
rennmaus-coder <christian@palm-family.de>
2022-10-29 19:24:59 +0200

    resolve conflict

--- PocketLearn/PocketLearn/Core/Learning/LearnProject.cs
+++ PocketLearn/PocketLearn/Core/Learning/LearnProject.cs
@@ -19,7 +19,6 @@ namespace PocketLearn.Core.Learning
         public List<LearnCard> Cards { get => _cards; set { _cards = value; } }
         public ProjectConfig ProjectConfig { get; set; }
 
-        [JsonProperty]
         public Guid ProjectID { get; private set; }
 
         private List<LearnCard> hardCards = new List<LearnCard>();
