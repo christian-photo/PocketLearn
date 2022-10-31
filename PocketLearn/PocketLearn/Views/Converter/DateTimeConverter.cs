69dc2710be3b81dbd8409d3746106a9ada9eddc1
rennmaus-coder <christian@palm-family.de>
2022-10-31 11:44:07 +0100

    converter

---
+++ PocketLearn/PocketLearn/Views/Converter/DateTimeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace PocketLearn.Views.Converter
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{parameter}: {(DateTime)value:dd.MM.yy}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
