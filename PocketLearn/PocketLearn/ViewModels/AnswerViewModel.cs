4264038aeaf48357fbafe4a925f07d5af89c77b5
alex3r45u <alexanderpalme07@gmail.com>
2022-10-31 12:56:32 +0100

    Question and Answer (not coplete finished)

--- PocketLearn/PocketLearn/ViewModels/AnswerViewModel.cs
+++ PocketLearn/PocketLearn/ViewModels/AnswerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PocketLearn.Shared.Core.Learning;
+using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -7,9 +8,21 @@ namespace PocketLearn.ViewModels
 {
     public class AnswerViewModel : BaseViewModel
     {
+        private CardContent _answerContent;
+        public CardContent AnswerContent { get => _answerContent; set => SetProperty(ref _answerContent, value); }
+
+        private CardContent _questionContent;
+        public CardContent QuestionContent { get => _questionContent; set => SetProperty(ref _questionContent, value); }
+
+
+        public LearnProject Project { get; set; }
         public AnswerViewModel()
         {
             Title = "AnswerVM";
+        }
+        public AnswerViewModel(LearnProject project)
+        {
+            Project = project;
         }
     }
 }
